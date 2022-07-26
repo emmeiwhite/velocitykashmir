1effce1d09765dc9c2c6ab25eb995bcb4b637d03
Imran Rafiq Rather <emmeiwhite@gmail.com>
2022-07-27 02:45:41 +0530

    Interative phone, whatsApp and email

---
+++ script.js
@@ -0,0 +1,26 @@
+// WhatsApp Chat
+/* Whatsapp Chat Widget by www.bloggermix.com */
+$(document).on("click", "#send-it", function () {
+  var a = document.getElementById("chat-input");
+  if ("" != a.value) {
+    var b = $("#get-number").text(),
+      c = document.getElementById("chat-input").value,
+      d = "https://web.whatsapp.com/send",
+      e = b,
+      f = "&text=" + c;
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    )
+      var d = "whatsapp://send";
+    var g = d + "?phone=" + e + f;
+    window.open(g, "_blank");
+  }
+}),
+  $(document).on("click", ".close-chat", function () {
+    $("#whatsapp-chat").addClass("hide").removeClass("show");
+  }),
+  $(document).on("click", ".blantershow-chat", function () {
+    $("#whatsapp-chat").addClass("show").removeClass("hide");
+  });
